f6c68bcb29a14ead4a0299d6aecbc904eca18694
tom_glb <lsy7895123aa@sina.com>
2020-05-02 00:58:11 +0800

    初始化项目

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component}from 'react';
+
+// function App() {
+//   return (
+//     <div className="App">
+//   </div>
+//   );
+// }
+class App extends Component{
+  render(){
+    return(
+      <div>app</div>
+    )
+  }
+}
+
+export default App;
